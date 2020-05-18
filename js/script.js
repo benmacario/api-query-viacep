9117fba179943bb24eccd8ea0a23bf59d4a4d716
Brendon Machado <benmacario.s@gmail.com>
2020-05-18 16:40:47 -0300

    app completed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 const cep = document.querySelector("#cep")
 
+const showData = (result) => {
+  for(const campo in result) {
+    if(document.querySelector(`#${campo}`)) {
+      const campos = document.querySelector(`#${campo}`)
+      campos.value = result[campo]
+    }
+
+  }
+}
+
 cep.addEventListener("blur", (e) => {
   let search = cep.value.replace("-","")
 
@@ -11,7 +21,7 @@ cep.addEventListener("blur", (e) => {
 
   fetch(`http://viacep.com.br/ws/${search}/json`, option)
   .then(res => {res.json()
-    .then(data => console.log(data))  
+    .then(data => showData(data))  
   })
   .catch(err => console.log("Deu erro: " + err))
 })
